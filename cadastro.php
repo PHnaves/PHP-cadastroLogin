b6d693f759b447a3b50885495276da316b31d795
PHnaves <pnaves001@gmail.com>
2024-10-31 22:20:25 -0300

    resolucao de bugs

--- cadastro.php
+++ cadastro.php
@@ -4,29 +4,26 @@ session_start();
 
 // obtendo e gaurdando as informacoes disponibilizados no formulario de cadastro
 $email = $_POST['email'];
-$nomeCompleto = $_POST['nome-completo'];
+$nomeCompleto = $_POST['nomeCompleto'];
 $telefone = $_POST['telefone'];
 $senha1 = $_POST['senha1'];
-$senhaConfirmada = $_POST['senha2'];
+$senha2 = $_POST['senha2'];
 
-if($senhaCriada == $senhaConfirmada) {
+if($senha2 =! $senha1) {
     echo "As senhas não correspondem!";
-    header("Refresh: 2; url=cadastro.html");
-    exit();
 }
 else{
     // criando a conexao com o banco de dados
-    $pdo = new PDO("mysql:host=localhost;dbname=alunos", "root", ""); 
-    $stmt = $pdo->prepare("INSERT INTO alunos (email, nomeCompleto, telefone, senha) VALUES (:email, :nomeCompleto, :telefone, :senhaConfirmada)");
+    $pdo = new PDO("mysql:host=localhost;dbname=alunos", 'root', 'root'); 
+    $stmt = $pdo->prepare("INSERT INTO informacoesalunos (email, nomeCompleto, telefone, senha) VALUES (:email, :nomeCompleto, :telefone, :senha)");
 
     $stmt ->bindParam(':email', $email);
     $stmt ->bindParam(':nomeCompleto', $nomeCompleto);
     $stmt ->bindParam(':telefone', $telefone);
-    $stmt ->bindParam(':senhaConfirmada', $senhaConfirmada);
+    $stmt ->bindParam(':senha', $senha1);
 
     if ($stmt->execute()) {
-        header("Location: login.php");
-        exit();
+        header("Location: login.html");
     }
 
     else{
--- login.php
+++ login.php
@@ -1,24 +1,34 @@
-<?php 
+<?php
 require 'cadastro.php';
 
-$email = $_POST['email'];
-$senha = $_post['password'];
+$dsn = 'mysql:host=localhost;dbname=alunos';
+$username = 'root';
+$password = 'root';
 
-$stmt = $pdo->prepare("SELECT id, email, nomeCompleto, telefone, senha FROM users WHERE email = :email AND senha = :senha ");
+try {
+    $pdo = new PDO($dsn, $username, $password);
+} catch (PDOException $e) {
+    echo 'Erro ao conectar com o banco de dados: ' . $e->getMessage();
+    exit;
+}
+
+$email = $_POST['email'];
+$senha = $_POST['password'];
+
+$stmt = $pdo->prepare("SELECT email, senha FROM informacoesalunos WHERE email = :email AND senha = :senha");
 $stmt->bindParam(':email', $email);
 $stmt->bindParam(':senha', $senha);
 
+$stmt->execute();
+
 $userData = $stmt->fetch(PDO::FETCH_ASSOC);
 
-if($email == $userData['email'] && $senha == $userData['senha']){
+if ($email == $userData['email'] && $senha == $userData['senha']) {
     session_start();
     $_SESSION['email'] = $email;
     $_SESSION['senha'] = $senha;
-    header("location: conteudo.php");
+    header("Location: conteudo.html");
+} else {
+    echo "Login ou senha inválidos!";
 }
-else{
-    echo "login ou senha invalidos!";
-}
-
-
 ?>
